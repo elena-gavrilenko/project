36f7e269131611089c07a596f3be270c875f9a9d
unknown <gomel-offis@mail.ru>
2021-07-29 21:30:17 +0300

    begin project

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hi");
+const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privet: false,
+};
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколько оцените его?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколько оцените его?", "");
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
