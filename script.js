2d197ba182b0fe611eb0c62a80125d6ea2252755
unknown <gomel-offis@mail.ru>
2021-08-18 17:55:01 +0300

    cycle

--- script.js
+++ script.js
@@ -6,14 +6,28 @@ const personalMovieDB = {
   genres: [],
   privet: false,
 };
-for (let i = 0; i < 2; i++) {
+/*for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");*/
+let i = 0;
+while (i < 2) {
   const a = prompt("Один из последних просмотренных фильмов?", ""),
     b = prompt("На сколько оцените его?", "");
 
   if (a.length < 50 && a != "" && b != "" && a != null && b != null) {
     personalMovieDB.movies[a] = b;
+    i++;
   } else {
     i--;
   }
 }
 console.log(personalMovieDB);
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
