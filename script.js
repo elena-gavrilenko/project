2591adfac5587665b2bf6e4670ccace07a901594
unknown <gomel-offis@mail.ru>
2021-08-29 18:47:27 +0300

    function

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+let numberOfFilms;
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+  while (numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms == "") {
+    numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+  }
+}
+start();
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -9,25 +16,48 @@ const personalMovieDB = {
 /*for (let i = 0; i < 2; i++) {
   const a = prompt("Один из последних просмотренных фильмов?", ""),
     b = prompt("На сколько оцените его?", "");*/
-let i = 0;
-while (i < 2) {
-  const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", "");
+function rememberMyFilms() {
+  let i = 0;
+  while (i < 2) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
 
-  if (a.length < 50 && a != "" && b != "" && a != null && b != null) {
-    personalMovieDB.movies[a] = b;
-    i++;
-  } else {
-    i--;
+    if (a.length < 50 && a != "" && b != "" && a != null && b != null) {
+      personalMovieDB.movies[a] = b;
+      i++;
+    } else {
+      i--;
+    }
   }
 }
-console.log(personalMovieDB);
-if (personalMovieDB.count < 10) {
-  console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-  console.log("Вы классический зритель");
-} else if (personalMovieDB.count > 30) {
-  console.log("Вы киноман");
-} else {
-  console.log("Произошла ошибка");
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
 }
+detectPersonalLevel();
+function showMyDB() {
+  if (!personalMovieDB.privet) {
+    console.log(personalMovieDB);
+  }
+}
+/*showMyDB();*/
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+  console.log(personalMovieDB);
+}
+writeYourGenres();
+const str = "А роза упала на лапу Азора";
+console.log(str.slice(7, 12));
+console.log(str.slice(21, 26));
+console.log(str.slice(7));
