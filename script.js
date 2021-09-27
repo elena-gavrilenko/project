6aea0c06677aee21d300bcf1da4790cecbf96045
unknown <gomel-offis@mail.ru>
2021-09-27 18:25:16 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,62 +1,76 @@
-// let numberOfFilms;
+// let personalMovieDB.count;
 // function start() {
-//   numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
-//   while (numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms == "") {
-//     numberOfFilms = +prompt("Сколько фильмов вы уже просмотрели?", "");
+//   personalMovieDB.count = +prompt("Сколько фильмов вы уже просмотрели?", "");
+//   while (personalMovieDB.count == null || isNaN(personalMovieDB.count) || personalMovieDB.count == "") {
+//     personalMovieDB.count = +prompt("Сколько фильмов вы уже просмотрели?", "");
 //   }
 // }
 // start();
-// const personalMovieDB = {
-//   count: numberOfFilms,
-//   movies: {},
-//   actors: {},
-//   genres: [],
-//   privet: false,
-// };
-// /*for (let i = 0; i < 2; i++) {
-//   const a = prompt("Один из последних просмотренных фильмов?", ""),
-//     b = prompt("На сколько оцените его?", "");*/
-// function rememberMyFilms() {
-//   let i = 0;
-//   while (i < 2) {
-//     const a = prompt("Один из последних просмотренных фильмов?", ""),
-//       b = prompt("На сколько оцените его?", "");
+const personalMovieDB = {
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  privet: false,
+  start: function () {
+    personalMovieDB.count = +prompt("Сколько фильмов вы уже просмотрели?", "");
+    while (
+      personalMovieDB.count == null ||
+      isNaN(personalMovieDB.count) ||
+      personalMovieDB.count == ""
+    ) {
+      personalMovieDB.count = +prompt(
+        "Сколько фильмов вы уже просмотрели?",
+        ""
+      );
+    }
+  },
+  rememberMyFilms: function () {
+    let i = 0;
+    while (i < 2) {
+      const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
 
-//     if (a.length < 50 && a != "" && b != "" && a != null && b != null) {
-//       personalMovieDB.movies[a] = b;
-//       i++;
-//     } else {
-//       i--;
-//     }
-//   }
-// }
-// rememberMyFilms();
-
-// function detectPersonalLevel() {
-//   if (personalMovieDB.count < 10) {
-//     console.log("Просмотрено довольно мало фильмов");
-//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-//     console.log("Вы классический зритель");
-//   } else if (personalMovieDB.count > 30) {
-//     console.log("Вы киноман");
-//   } else {
-//     console.log("Произошла ошибка");
-//   }
-// }
-// detectPersonalLevel();
-// function showMyDB() {
-//   if (!personalMovieDB.privet) {
-//     console.log(personalMovieDB);
-//   }
-// }
-// /*showMyDB();*/
-// function writeYourGenres() {
-//   for (let i = 1; i <= 3; i++) {
-//     personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-//   }
-//   console.log(personalMovieDB);
-// }
-// writeYourGenres();
+      if (a.length < 50 && a != "" && b != "" && a != null && b != null) {
+        personalMovieDB.movies[a] = b;
+        i++;
+      } else {
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMovieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+      console.log("Вы классический зритель");
+    } else if (personalMovieDB.count > 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  showMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMovieDB);
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+      personalMovieDB.genres[i - 1] = prompt(
+        `Ваш любимый жанр под номером ${i}`
+      );
+    }
+    console.log(personalMovieDB);
+  },
+};
+personalMovieDB.start();
+console.log(personalMovieDB.count);
+personalMovieDB.rememberMyFilms();
+console.log(personalMovieDB.movies);
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB(hidden);
+personalMovieDB.writeYourGenres();
 // const str = "А роза упала на лапу Азора";
 // console.log(str.slice(7, 12));
 // console.log(str.slice(21, 26));
@@ -87,17 +101,17 @@
 // // console.log(counter);
 // const { border, bg } = option.color;
 // console.log(border);
-const obj = {
-  a: "big",
-  b: "small",
-  c: "among",
-  d: {
-    f: "red",
-    j: "blue",
-  },
-};
-objNew = { ...obj };
-objNew.a = "biggest";
-objNew.d.f = "yellow";
-console.log(objNew);
-console.log(obj);
+// const obj = {
+//   a: "big",
+//   b: "small",
+//   c: "among",
+//   d: {
+//     f: "red",
+//     j: "blue",
+//   },
+// };
+// objNew = { ...obj };
+// objNew.a = "biggest";
+// objNew.d.f = "yellow";
+// console.log(objNew);
+// console.log(obj);
